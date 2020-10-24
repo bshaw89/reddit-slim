712d4f1eb119a9c275381c216c304c6fb80a198d
Benjamin Shaw <shaw.j.benjamin@gmail.com>
2020-10-23 20:44:00 -0400

    Initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MikroORM } from '@mikro-orm/core';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+import microConfig from './mikro-orm.config';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+
+  const post = orm.em.create(Post, {title: 'my first post'});
+  await orm.em.persistAndFlush(post);
+}
+
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
