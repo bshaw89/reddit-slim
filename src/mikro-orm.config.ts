07655f8263dc7089db307948b0afeb5e2ce1dc6c
Benjamin Shaw <shaw.j.benjamin@gmail.com>
2020-10-27 12:02:28 -0400

    Set up MikroORM

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -5,11 +5,11 @@ import path from 'path';
 
 export default {
   migrations: {
-    path: path.join(__dirname, './migrations'),
-    pattern: /^[\w-]+\d+\.[tj]s$/ // regular expression [tj] handling javascript as well as typescript
+    path: path.join(__dirname, './migrations'), // path to the folder with migrations; joins these two filepaths
+    pattern: /^[\w-]+\d+\.[tj]s$/, // altered regular expression to also handle js
   },
   entities: [Post],
-  dbName: 'reddit-slim',
-  type: 'postgresql',
-  debug: !__prod__,
+    dbName: "reddit-slim",
+    type: 'postgresql',
+    debug: !__prod__
 } as Parameters<typeof MikroORM.init>[0];
